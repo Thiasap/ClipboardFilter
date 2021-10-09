987401e1db38493b6d041f564075782c71701cda
Thiasap <799721744@qq.com>
2021-10-09 20:41:13 +0800

    使用XSharedPreferences做配置文件，添加日志开关

--- app/src/main/java/com/bit747/clipboardfilter/MainActivity.java
+++ app/src/main/java/com/bit747/clipboardfilter/MainActivity.java
@@ -1,53 +1,153 @@
 package com.bit747.clipboardfilter;
 
-
 import android.app.Activity;
-import android.opengl.ETC1;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.regex.Pattern;
 
-import de.robv.android.xposed.XposedBridge;
+public class MainActivity extends Activity implements View.OnClickListener{
 
-public class MainActivity extends Activity {
-
+    Button bt,saveRules;
+    EditText ruleE;
+    EditText content;
+    SharedPreferences sp;
+    Context context;
+    String rules;
+    CheckBox LogEnable,LogDetails,LogAll;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button bt = findViewById(R.id.goTest);
-        bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText ruleE = findViewById(R.id.rules);
-                EditText content = findViewById(R.id.content);
+        init();
+    }
+    String encode(String str){
+        return str.replaceAll("\\\\","\\\\\\\\").trim();
+    }
+    String decode(String str){
+        return str.replaceAll("\\\\\\\\","\\\\").trim();
+    }
+    public void init(){
+        ruleE = findViewById(R.id.rules);
+        content = findViewById(R.id.content);
+        context = this;
+        sp = context.getSharedPreferences("rules", Context.MODE_WORLD_READABLE);
+        //sp = context.getSharedPreferences("rules", Context.MODE_PRIVATE);
+        rules = decode(sp.getString("rules",""));
+        if ("".equals(rules)) rules="(1.fu:).*\n^(\\d+:/\\^).*.(\\^)$\n^(\\$\\w+@.?\\w+).*.(\\$)$";
+        ruleE.setText(rules);
+        bt = findViewById(R.id.goTest);
+        saveRules = findViewById(R.id.saveRules);
+        LogEnable = findViewById(R.id.LogEnable);
+        LogAll = findViewById(R.id.LogAll);
+        LogDetails = findViewById(R.id.LogDetails);
+        bt.setOnClickListener(this);
+        saveRules.setOnClickListener(this);
+        LogEnable.setOnClickListener(this);
+        LogAll.setOnClickListener(this);
+        LogDetails.setOnClickListener(this);
+        LogEnable.setChecked(sp.getBoolean("LogEnable",false));
+        LogAll.setChecked(sp.getBoolean("LogAll",false));
+        LogDetails.setChecked(sp.getBoolean("LogDetails",false));
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.saveRules:
+                saveRules();
+                break;
+            case R.id.goTest:
                 String ct = content.getText().toString();
-                boolean isMatch = false;
-                String[] rules = (ruleE.getText().toString()).split("\n");
-                try{
-                    for(String rule:rules){
-                        if(Pattern.matches(rule, ct)){
-                            isMatch=true;
-                            break;
-                        }
-
-                    }
-                    if(isMatch){
-                        Toast.makeText(MainActivity.this,"匹配成功！",Toast.LENGTH_SHORT).show();
-                    }else{
-                        Toast.makeText(MainActivity.this,"匹配失败！",Toast.LENGTH_SHORT).show();
-                    }
-                }catch(Exception e) {
-                    Toast.makeText(MainActivity.this,"正则语句出错了！",Toast.LENGTH_SHORT).show();
+                rules = ruleE.getText().toString();
+                checkRule(ct,rules.split("\n"),context);
+                break;
+            case R.id.LogEnable:
+                CBLog(R.id.LogEnable);
+                break;
+            case R.id.LogDetails:
+                CBLog(R.id.LogDetails);
+                break;
+            case R.id.LogAll:
+                CBLog(R.id.LogAll);
+                break;
+            default:
+                break;
+        }
+    }
+    public void toast(String msg){
+        Toast.makeText(context,msg,Toast.LENGTH_SHORT).show();
+    }
+    public void CBLog(int id){
+        CheckBox cb = findViewById(id);
+        SharedPreferences.Editor editor = sp.edit();
+        switch (id){
+            case R.id.LogEnable:
+                editor.putBoolean("LogEnable",cb.isChecked());
+                toast("开启日志功能");
+                break;
+            case R.id.LogDetails:
+                editor.putBoolean("LogDetails",cb.isChecked());
+                toast("开启详细日志");
+                break;
+            case R.id.LogAll:
+                editor.putBoolean("LogAll",cb.isChecked());
+                toast("开启所有日志");
+                break;
+            default:
+                break;
+        }
+        editor.commit();
+    }
+    public void saveRules(){
+        final AlertDialog.Builder alterDiaglog = new AlertDialog.Builder(MainActivity.this);
+        alterDiaglog.setTitle("保存规则");
+        alterDiaglog.setMessage("是否覆盖已存在的规则？");
+        alterDiaglog.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                rules = ruleE.getText().toString();
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("rules",encode(rules));
+                editor.commit();
+                if((sp.getString("rules","")).equals(encode(rules))){
+                    Toast.makeText(context,"保存成功！",Toast.LENGTH_SHORT).show();
                 }
-
 
             }
         });
+        alterDiaglog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        alterDiaglog.show();
+    }
+
+    public void checkRule(String ct, String[] rules, Context ctx){
+        boolean isMatch = false;
+        try{
+            for(String rule:rules){
+                if(Pattern.matches(rule, ct)){
+                    isMatch=true;
+                    break;
+                }
+            }
+            if(isMatch){
+                Toast.makeText(ctx,"匹配成功！",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(ctx,"匹配失败！",Toast.LENGTH_SHORT).show();
+            }
+        }catch(Exception e) {
+            Toast.makeText(ctx,"正则语句出错了！",Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/bit747/clipboardfilter/xposedInit.java
+++ app/src/main/java/com/bit747/clipboardfilter/xposedInit.java
@@ -2,78 +2,84 @@ package com.bit747.clipboardfilter;
 
 import android.content.ClipData;
 import android.content.ClipboardManager;
-import android.os.Environment;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Pattern;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class xposedInit implements IXposedHookLoadPackage{
     private final String TAG = "剪切板过滤器：";
-    private final String CONF = "/sdcard/Android/hooker/clipboardfilter.conf";
-    private final String CUSTOM_RULE="";
+    private static XSharedPreferences getPref(String path) {
+        XSharedPreferences pref = new XSharedPreferences(BuildConfig.APPLICATION_ID, path);
+        return pref.getFile().canRead() ? pref : null;
+    }
+    String decode(String str){
+        return str.replaceAll("\\\\\\\\","\\\\").trim();
+    }
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        File conf = new File(CONF);
-        if(conf.exists()||!CUSTOM_RULE.equals("")){
-            XposedHelpers.findAndHookMethod(ClipboardManager.class, "setPrimaryClip",
-                    ClipData.class, new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            super.beforeHookedMethod(param);
-                            // 获取剪切板内容
-                            ClipData clipData = (ClipData) param.args[0];
-                            String clipStr = clipData.getItemAt(0).getText().toString();
-                            //正则匹配
-                            boolean isExist = false;
-                            try {
-                                if(CUSTOM_RULE.equals("")){
-                                    List<String> rules = new ArrayList<>();
-                                    if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
-                                        InputStream instream = new FileInputStream(conf);
-                                        if (instream != null) {
-                                            InputStreamReader inputreader = new InputStreamReader(instream);
-                                            BufferedReader buffreader = new BufferedReader(inputreader);
-                                            String line;
-                                            //分行读取
-                                            while (( line = buffreader.readLine()) != null) {
-                                                rules.add(line);
-                                            }
-                                            instream.close();
-                                        }
-                                    }
-                                    for(String rule:rules){
-                                        if(Pattern.matches(rule, clipStr)){
-                                            isExist = true;
-                                            XposedBridge.log(TAG + "过滤成功");
-                                            break;
-                                        }
-                                    }
-                                }else {
-                                    isExist = Pattern.matches(CUSTOM_RULE, clipStr);
-                                }
-                            }catch(Exception e){
-                                isExist = false;
-                            }
-                            if (isExist){
-                                ClipData c= ClipData.newPlainText("","");
-                                param.args[0] = c;
-                            }
-                        }
-                    });
+        if(lpparam.packageName.equals(BuildConfig.APPLICATION_ID)) return;
+        XSharedPreferences pref = getPref("rules");
+        if (pref == null) {
+            XposedBridge.log(TAG+"【Error】Cannot load pref");
+            return;
         }
+        String rules_r = decode(pref.getString("rules", ""));
+
+        String[] rules = rules_r.split("\n");
+        if("".equals(rules_r)) return;
+        XposedHelpers.findAndHookMethod(ClipboardManager.class, "setPrimaryClip",
+                ClipData.class, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        super.beforeHookedMethod(param);
+                        // 获取剪切板内容
+                        ClipData clipData = (ClipData) param.args[0];
+                        String clipStr = clipData.getItemAt(0).getText().toString();
+                        if("".equals(clipStr)) return;
+                        // 获取应用名
+                        String appName = lpparam.packageName;
+                        //正则匹配
+                        boolean isExist = false;
+                        try {
+                            for(String rule:rules){
+                                if(Pattern.matches(rule, clipStr)){
+                                    isExist = true;
+                                    break;
+                                }
+                            }
+                        }catch(Exception e){
+                            isExist = false;
+                        }
+                        boolean LogEnable = pref.getBoolean("LogEnable",false);
+                        if(LogEnable){
+                            boolean LogAll = pref.getBoolean("LogAll",false);
+                            if ((!LogAll)&&(!isExist)) return;
+                            String log=TAG+appName+" 写入了剪贴板。";
+                            log = log+(isExist?"【已过滤】":"");
+                            boolean LogDetails = pref.getBoolean("LogDetails",false);
+                            if (LogDetails){
+                                log = log + "内容：" + clipStr;
+                            }
+                            XposedBridge.log(log);
+                            //boolean LogAll = pref.getBoolean("LogAll",false);
+
+                        }
+
+                        if (isExist){
+                            ClipData c= ClipData.newPlainText("","");
+                            param.args[0] = c;
+                        }
+                    }
+                });
 
     }
+
+
 
 }
